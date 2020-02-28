c78e9d5c877f4e48798a9af1b3c565475faab0ba
tld2512 <tienlongdinh251297@gmail.com>
2020-02-28 16:33:03 +0700

    create tag repo

---
+++ src/main/java/com/longdt/repository/ImageBlogRepository.java
@@ -0,0 +1,4 @@
+package com.longdt.repository;
+
+public class ImageBlogRepository {
+}
---
+++ src/main/java/com/longdt/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.longdt.repository;
+
+import com.longdt.model.tag.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
